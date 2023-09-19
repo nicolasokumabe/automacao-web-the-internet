9f07ddbd1eb6707e1c5e3e86d8e1d8f0d50be250
Nicolas Kumabe <nicolas.kumabe@gmail.com>
2023-09-19 15:53:31 -0300

    Meu primeiro commit

---
+++ web/features/basic_auth.feature
@@ -0,0 +1,9 @@
+#language: pt
+
+Funcionalidade: Login
+    @temp
+    Cenario: Fazer login
+
+        Dado que acesso a página Basic Auth
+        Quando submeto o meu login completo
+        Então sou redirecionado para o Dashboard
---
+++ web/features/step_definitions/login_steps.rb
@@ -0,0 +1,11 @@
+Dado("que acesso a página Basic Auth") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Quando("submeto o meu login completo") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Então("sou redirecionado para o Dashboard") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
