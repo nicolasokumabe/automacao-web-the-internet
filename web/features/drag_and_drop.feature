f58f3216bf4bf486f2510fa920ca300602783645
Nicolas Kumabe <nicolas.kumabe@gmail.com>
2023-09-23 10:26:47 -0300

    Adicionado Drag and Drop

---
+++ web/features/drag_and_drop.feature
@@ -0,0 +1,13 @@
+#language: pt
+
+Funcionalidade: Arrastar e Soltar
+
+  Cenário: Arrastar um elemento A para o local do elemento B
+    Dado que acesso a página de Arrastar e Soltar
+    Quando arrasto o elemento A para o local do elemento B
+    Então devo verificar que o elemento A foi movido com sucesso para o local do elemento B
+
+Cenário: Arrastar um elemento B para o local do elemento A
+    Dado que acesso a página de Arrastar e Soltar
+    Quando arrasto o elemento B para o local do elemento A
+    Então devo verificar que o elemento B foi movido com sucesso para o local do elemento A
---
+++ web/features/step_definitions/drag_and_drop.rb
@@ -0,0 +1,25 @@
+Dado("que acesso a página de Arrastar e Soltar") do
+  visit "http://the-internet.herokuapp.com/drag_and_drop"
+end
+
+Quando("arrasto o elemento A para o local do elemento B") do
+  element_a = find("#column-a")
+  element_b = find("#column-b")
+
+  element_a.drag_to(element_b)
+end
+
+Então("devo verificar que o elemento A foi movido com sucesso para o local do elemento B") do
+  expect(page).to have_selector("#column-b", text: "A")
+end
+
+Quando("arrasto o elemento B para o local do elemento A") do
+  element_a = find("#column-a")
+  element_b = find("#column-b")
+
+  element_b.drag_to(element_a)
+end
+
+Então("devo verificar que o elemento B foi movido com sucesso para o local do elemento A") do
+  expect(page).to have_selector("#column-a", text: "B")
+end
