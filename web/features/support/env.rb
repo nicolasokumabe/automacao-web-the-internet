1349c2f93f29d34c3173101e10933cba55cf1c53
Nicolas Kumabe <nicolas.kumabe@gmail.com>
2023-09-25 20:46:39 -0300

    Adicionado Hooks

--- web/features/support/env.rb
+++ web/features/support/env.rb
@@ -5,11 +5,8 @@ require "dotenv/load"
 require "digest"
 require "rest-client"
 require_relative "login"
+require_relative "hooks"
 
 Capybara.configure do |config|
   config.default_driver = :selenium_chrome
-end
-
-Before do
-  Capybara.current_session.driver.browser.manage.delete_all_cookies
 end
--- web/features/support/hooks.rb
+++ web/features/support/hooks.rb
@@ -1,3 +1,4 @@
-# Before do |scenario|
-#   Capybara.reset!
-# end
+Before do
+  Capybara.reset!
+  Capybara.current_session.driver.browser.manage.delete_all_cookies
+end
