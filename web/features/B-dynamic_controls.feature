56437c52a94a2b112a7d4e3b39bf8242469acd5e
Nicolas Kumabe <nicolas.kumabe@gmail.com>
2023-09-27 17:25:51 -0300

    Ajuste Dynamic Controls 1.1

--- web/features/B-dynamic_controls.feature
+++ web/features/B-dynamic_controls.feature
@@ -1,23 +1,30 @@
-#language: pt
-
+# language: pt
 Funcionalidade: Controles Dinâmicos
 
-  Cenario: Habilitar e desabilitar campo de entrada
+  @t
+  Cenário: Habilitar e desabilitar campo de entrada
     Dado que estou na página de Dynamic Controls
-    Quando eu clico no botão "Enable"
+    Quando eu clico no botão Enable
     Então o campo de entrada deve estar habilitado
+    E eu clico no botão Disable
     E o campo de entrada deve estar desabilitado
-@t
-  Cenário: Remover a caixa de seleção
-    Dado que estou na página de Dynamic Controls
-    Quando eu clico no botão "Remove"
-    E aguardo a caixa de seleção ser removida
-    Então a caixa de seleção não deve ser visível
 
-@t
-  Cenário: Adicionar a caixa de seleção
-    Dado que estou na página de Dynamic Controls
-    Quando eu aguardo o botão "Add" ficar habilitado
-    E eu clico no botão "Add"
-    E aguardo a caixa de seleção ser adicionada
-    Então a caixa de seleção deve ser adicionada novamente
+    # Cenário: Habilitar e desabilitar campo de entrada
+    # Dado que estou na página de Dynamic Controls
+    # Quando eu clico no botão Enable
+    # Então o campo de entrada deve estar habilitado
+    # E eu clico no botão Disable
+    # E o campo de entrada deve estar desabilitado
+
+  # Cenário: Remover a caixa de seleção
+  #   Dado que estou na página de Dynamic Controls
+  #   Quando eu clico no botão Remover
+  #   E aguardo a caixa de seleção ser removida com sucesso
+  #   Então a caixa de seleção não deve ser visível
+  # @t
+  # Cenário: Adicionar a caixa de seleção
+  #   Dado que estou na página de Dynamic Controls
+  #   Quando eu aguardo o botão Adicionar ficar habilitado
+  #   E eu clico no botão Adicionar
+  #   E aguardo a caixa de seleção ser adicionada com sucesso
+  #   Então a caixa de seleção deve ser adicionada novamente
--- web/features/step_definitions/dynamic_controls.rb
+++ web/features/step_definitions/dynamic_controls.rb
@@ -2,50 +2,31 @@ Dado("que estou na página de Dynamic Controls") do
   visit "http://the-internet.herokuapp.com/dynamic_controls"
 end
 
-Quando("eu clico no botão {string}") do |botao|
-  if botao == "Remove"
-    execute_script("document.querySelector('button[onclick=\"swapCheckbox()\"]').click()")
-  else
-    click_button(botao)
-  end
+Quando("eu clico no botão Enable") do
+  click_button "Enable"
 end
 
 Então("o campo de entrada deve estar habilitado") do
-  campo = find("#input-example input")
-  expect(campo).to be_visible
+  find("#loading")
+  Capybara.default_max_wait_time = 5
+  expect(page).to have_css("p#message", text: "It's enabled!")
+
+  Capybara.default_max_wait_time = 5
+  # expect(page).to have_no_css('input[type="text"][disabled]')
+  input = find('input[type="text"][style*="width: 30%"]:not([disabled])')
+  input.set("pwd123")
+end
+
+Então("eu clico no botão Disable") do
+  click_button "Disable"
 end
 
 Então("o campo de entrada deve estar desabilitado") do
-  campo = find("#input-example input")
-  expect(campo).to be_visible
-  expect(campo).to be_disabled
-end
+  find("#loading")
+  Capybara.default_max_wait_time = 5
+  expect(page).to have_css("p#message", text: "It's disabled!")
 
-Então("a caixa de seleção deve ser removida") do
-  expect(page).not_to have_selector("#checkbox", visible: true)
-end
-
-E("aguardo a caixa de seleção ser removida") do
-  expect(page).not_to have_selector("#checkbox", visible: true)
-end
-
-Então("a caixa de seleção não deve ser visível") do
-  expect(page).not_to have_selector("#checkbox", visible: true)
-end
-
-Quando("eu aguardo o botão {string} ficar habilitado") do |botao|
-  expect(page).to have_button(botao, disabled: false)
-end
-
-E("eu clico no botão {string}") do |botao|
-  click_button(botao)
-end
-
-Quando("aguardo a caixa de seleção ser adicionada") do
-  sleep 3
-  expect(page).to have_selector("#checkbox", visible: true)
-end
-
-Então("a caixa de seleção deve ser adicionada novamente") do
-  expect(page).to have_selector("#checkbox", visible: true)
+  Capybara.default_max_wait_time = 5
+  input = find('input[type="text"]')
+  expect(page).to have_css('input[type="text"][disabled]')
 end
