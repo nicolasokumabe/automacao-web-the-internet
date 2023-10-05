60f925f883bf90b787c2a74b638d8868e97fca05
Nicolas Kumabe <nicolas.kumabe@gmail.com>
2023-10-05 12:07:03 -0300

    Adicionado Javascript Error

---
+++ web/features/step_definitions/javascript_error.rb
@@ -0,0 +1,3 @@
+Então("devo ver a mensagem de erro de JavaScript") do
+  expect(page).to have_content("This page has a JavaScript error in the onload event. This is often a problem to using normal Javascript injection techniques.")
+end
