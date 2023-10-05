95d6499e8e0229c8fc5f8199172c2fea25a0f335
Nicolas Kumabe <nicolas.kumabe@gmail.com>
2023-10-05 16:00:35 -0300

    Adicionado Large & Deep DOM

---
+++ web/features/step_definitions/large.rb
@@ -0,0 +1,10 @@
+Então("devo verificar se um elemento com id {string} existe no DOM") do |element_id|
+  page.execute_script("window.scrollBy(0, 10000)")
+  page.execute_script("window.scrollBy(10000, 0)")
+
+  row_50 = find(".row-50")
+  column_50 = row_50.find(".column-50")
+  texto = column_50.text
+
+  texto == element_id
+end
